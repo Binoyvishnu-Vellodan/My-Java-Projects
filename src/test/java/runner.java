40eafe04f85219bdbec2746f3884d5a525275e7d
binoyvishnuvellodan <rkobinoyvishnu@gmail.com>
2024-08-22 12:07:50 -0400

    test Data Change

--- src/test/java/runner.java
+++ src/test/java/runner.java
@@ -1,2 +1,27 @@
+import com.opencsv.exceptions.CsvException;
+import org.example.DataReader;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class runner {
+
+public static void main(String args[]) {
+    String excelFilePath = "C:\\Users\\rkobinoyvishnu\\IdeaProjects\\Java\\src\\test\\Docs\\Data.xlsx";
+    String csvFilePath = "C:\\Users\\rkobinoyvishnu\\IdeaProjects\\Java\\src\\test\\Docs\\Locator.csv";
+    try {
+        Map<String, Map<String, String>> testData = DataReader.getTestData(excelFilePath, csvFilePath);
+        String testCasenumber = "TC01";
+        if (testData.containsKey(testCasenumber)) {
+            System.out.println("Test Data for " + testCasenumber + ": " + testData.get(testCasenumber));
+        } else {
+//                System.out.println("Test case " + testCaseNumber + " not found.");
+//            }
+        }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    } catch (CsvException e) {
+        throw new RuntimeException(e);
+    }
+}
 }
