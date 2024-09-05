10d6d7774843814fc05e293a68a33277fe14eb97
binoyvishnuvellodan <rkobinoyvishnu@gmail.com>
2024-09-05 11:29:04 -0400

    new changes

--- src/main/java/org/example/Number_of_repeated_substring_ina__String.java
+++ src/main/java/org/example/Number_of_repeated_substring_ina__String.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.openqa.selenium.json.JsonOutput;
-
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,8 +8,19 @@ import java.util.Map;
 
 public class Number_of_repeated_substring_ina__String {
 
-    public static void d(String args[]) {
+    //calling all the methods related to the string actions performed and show its result
+    public static void main(String[] args) {
         String Str = "I like munnar and munnar is a beautiful place and munnar is a hilll station";
+        Integer mostRepeatedWordCount= mostRepeatedWordCount(Str);
+        String MostRepeatedWord= MostRepeatedWord(Str);
+        String MostRepeatedString= MostRepeatedString(Str);
+        System.out.println("The most repeated word count = "+ mostRepeatedWordCount);
+        System.out.println("The most repeated word is = "+ MostRepeatedWord);
+        System.out.println("The most repeated string is = "+ MostRepeatedString);
+    }
+
+        //method returns the most Repeated Word Count
+        public static Integer mostRepeatedWordCount(String Str) {
         String[] words=Str.trim().split("\\s+");
         int Repeatedword=0;
         int wordcount= words.length;
@@ -20,19 +30,41 @@ public class Number_of_repeated_substring_ina__String {
                 Repeatedword=Repeatedword+1;
             }
         }
-        System.out.println("Repeated word count = "+ Repeatedword );
+        return Repeatedword;
     }
-    public static  void main(String args[]){
-        String Str= "I like munnar and munnar is a beautiful place and munnar is a hilll station";
-        String[] words=Str.split(" ");
-        Map<String, String> datamap= new HashMap<>();
-        String mostrepeatedString=null;
 
-//        String[] wordcout=Str.trim().split("\\s+");
-//        for(int i=0;i<wordcout;i++){
-//            arr = arr.add(i);
-
-//        }
-
+    public static  String MostRepeatedWord(String Str) {
+    String[] words=Str.trim().split("\\s");
+    Map<String, Integer> wordcount= new HashMap<>();
+    for(String word: words){
+        wordcount.put(word,wordcount.getOrDefault(word,0)+1);
+    }
+    String mostrepeated= null;
+    int maxcount=0;
+    for(Map.Entry<String,Integer> entry: wordcount.entrySet()){
+        if(entry.getValue()>maxcount){
+            maxcount=entry.getValue();
+            mostrepeated=entry.getKey();
+        }
+    }
+    return mostrepeated;
+    }
+    public static  String MostRepeatedString(String Str) {
+        Map<String, Integer> substringCount = new HashMap<>();
+        for(int length=2;length<=Str.length();length++){
+            for(int i=0;i<=Str.length()- length;i++){
+                String substring= Str.substring(i,i+length);
+                substringCount.put(substring,substringCount.getOrDefault(substring,0)+1);
+            }
+        }
+        String mostrepeated = "";
+        int maxcount = 0;
+        for (Map.Entry<String, Integer> entry : substringCount.entrySet()) {
+            if (entry.getValue() > maxcount ) {
+                mostrepeated = entry.getKey();
+                maxcount = entry.getValue();
+            }
+        }
+        return mostrepeated;
     }
 }
--- src/main/java/org/example/Sample.java
+++ src/main/java/org/example/Sample.java
@@ -1,12 +1,28 @@
 package org.example;
 
+import org.apache.commons.compress.utils.OsgiUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Scanner;
+
 public class Sample extends Selenium_Link{
-        public void waitForElement (){
-            WebDriverWait wait = new WebDriverWait(driver, 30);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@id,'swiper-wrapper-')and string-length(substring(@id, 16)) = 16]/descendant::a[text()='Explore Now']")));
+        public static void main(String args[]){
+            System.out.println("Enter the season of the year: ");
+            Scanner scanner= new Scanner(System.in);
+            String Season= scanner.next();
+            System.out.println("Enter the whole number: ");
+            double number= scanner.nextFloat();
+            System.out.println("Enter the adjective: ");
+            String adjective= scanner.next();
+            scanner.close();
+
+            System.out.println("On a "+adjective+" "+Season+" day, I drink a minimum of "+number+" cups of coffee");
+
+
+
+//            WebDriverWait wait = new WebDriverWait(driver, 30);
+//            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@id,'swiper-wrapper-')and string-length(substring(@id, 16)) = 16]/descendant::a[text()='Explore Now']")));
         }
 }
